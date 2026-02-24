6c757e2012849265d02849bf096c886d33383a9f
Suhas-alagi <suhasalagi20@gmail.com>
2026-02-24 23:27:19 +0530

    SumOfDigit File Added

---
+++ NumberSystem/SumOfDigit.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class SumOfDigit{
+
+    public static void main(String args[]){
+        int num;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a Number");
+        num=sc.nextInt();
+        int count=0;
+
+        int sum=0;
+
+        //sum and count of num
+
+        while(num!=0){
+
+            sum=sum+(num%10);
+            num=num/10;
+
+            count++;
+            
+        }
+        System.out.println(count);
+        System.out.println("sum = "+sum);
+
+
+
+    }
+}
